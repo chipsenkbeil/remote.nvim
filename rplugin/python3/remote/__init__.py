2a6108fbf9d925c975d09aace492374debec4f32
Chip Senkbeil <chip.senkbeil@gmail.com>
2018-04-28 11:39:08 -0500

    Added skeleton plugin

---
+++ rplugin/python3/remote/__init__.py
@@ -0,0 +1,28 @@
+# =============================================================================
+# FILE: __init__.py
+# AUTHOR: Chip Senkbeil <chip.senkbeil at gmail.com>
+# License: Apache 2.0 License
+# =============================================================================
+
+import neovim
+
+
+@neovim.plugin
+class RemoteHandlers(object):
+    def __init__(self, nvim):
+        self.nvim = nvim
+
+    @neovim.command('RemoteConnect', nargs='*', range='')
+    def cmd_remote_connect(self, args, range):
+        self.nvim.current.line = ('RemoteConnect with args: {}, range: {}'
+                                  .format(args, range))
+
+    @neovim.command('RemoteListen', nargs='*', range='')
+    def cmd_remote_listen(self, args, range):
+        self.nvim.current.line = ('RemoteListen with args: {}, range: {}'
+                                  .format(args, range))
+
+    @neovim.autocmd('BufWrite', pattern='*', eval='expand("<afile>")', sync=True)
+    def on_bufwrite(self, filename):
+        self.nvim.out_write('Writing to ' + filename)
+
