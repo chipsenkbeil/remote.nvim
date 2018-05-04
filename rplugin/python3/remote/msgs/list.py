57f4c33d325d4f37153310ba4fc27ae44aea9782
Chip Senkbeil <chip.senkbeil@gmail.com>
2018-05-04 18:08:30 +0100

    Added list message (no tests)

---
+++ rplugin/python3/remote/msgs/list.py
@@ -0,0 +1,57 @@
+# =============================================================================
+# FILE: list.py
+# AUTHOR: Chip Senkbeil <chip.senkbeil at gmail.com>
+# License: Apache 2.0 License
+# =============================================================================
+from remote.msg import *
+
+# Represents the type of message
+MESSAGE_TYPE = 'LIST'
+
+
+def new(
+    username,
+    session,
+    path,
+    parent_header=Header.empty(),
+):
+    """Creates a new list message to execute on the remote nvim instance.
+
+    :param username: The name of the user sending the message
+    :param session: The session associated with the message
+    :param path: The path (relative to nvim) to use as the root of files and
+                 directories whose names to retrieve
+    :param parent_header: Optional parent header to indicate what led to
+                          sending the message
+    :returns: The new message instance
+    """
+    assert isinstance(path, str)
+    return (Message()
+            .set_header(Header()
+                        .set_random_msg_id()
+                        .set_username(username)
+                        .set_session(session)
+                        .set_date_now()
+                        .set_msg_type(MESSAGE_TYPE)
+                        .set_version(MESSAGE_VERSION))
+            .set_parent_header(parent_header)
+            .set_metadata(Metadata())
+            .set_content(Content().set_data(path)))
+
+
+def is_match(message):
+    """Checks if the provided message is a list message.
+
+    :param message: The message to check
+    :returns: True if a list message, otherwise False
+    """
+    return message.get_header().get_msg_type() == MESSAGE_TYPE
+
+
+def get_path(message):
+    """Returns the path of the message.
+
+    :param message: The message to check
+    :returns: The path to the directory whose files to list
+    """
+    return message.get_content().get_data()
