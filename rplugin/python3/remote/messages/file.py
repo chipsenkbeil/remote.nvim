b82c5a90eccd1756b8e7238699ae71468a5d6478
Chip Senkbeil <chip.senkbeil@gmail.com>
2018-05-04 22:01:03 +0100

    Added other file tests

--- rplugin/python3/remote/messages/file.py
+++ rplugin/python3/remote/messages/file.py
@@ -10,7 +10,8 @@ from .base import *
 # Represents the types of messages request/response/broadcast
 MESSAGE_TYPE_FILE_LIST = 'FILE_LIST'
 MESSAGE_TYPE_RETRIEVE_FILE = 'RETRIEVE_FILE'
-MESSAGE_TYPE_UPDATE_FILE = 'UPDATE_FILE'
+MESSAGE_TYPE_UPDATE_FILE_START = 'UPDATE_FILE_START'
+MESSAGE_TYPE_UPDATE_FILE_DATA = 'UPDATE_FILE_DATA'
 MESSAGE_TYPE_FILE_CHANGED = 'FILE_CHANGED'
 
 # Common metadata across file information
@@ -29,6 +30,7 @@ MESSAGE_DEFAULT_FILE_VERSION = 0
 MESSAGE_DEFAULT_TOTAL_CHUNKS = 1
 MESSAGE_DEFAULT_CHUNK_INDEX = 0
 MESSAGE_DEFAULT_CHUNK_DATA = b''
+MESSAGE_DEFAULT_CHUNKS_RECEIVED = 0
 
 
 class FileListRequestMessage(BaseRequestMessage):
@@ -226,6 +228,261 @@ class RetrieveFileResponseMessage(BaseResponseMessage):
             chunk_index=packet.get_metadata().get_value(MESSAGE_METADATA_CHUNK_INDEX),
             total_chunks=packet.get_metadata().get_value(MESSAGE_METADATA_TOTAL_CHUNKS),
             chunk_data=packet.get_content().get_data(),
+            parent=None,
+        )
+
+
+class UpdateFileStartRequestMessage(BaseRequestMessage):
+    _type = MESSAGE_TYPE_UPDATE_FILE_START
+    _file_path = None
+    _file_version = None
+
+    def __init__(
+        self,
+        id=str(uuid4()),
+        username=MESSAGE_DEFAULT_USERNAME,
+        session=MESSAGE_DEFAULT_SESSION,
+        file_path=MESSAGE_DEFAULT_FILE_PATH,
+        file_version=MESSAGE_DEFAULT_FILE_VERSION,
+        parent=None
+    ):
+        super().__init__(id, username, session, parent)
+        self._file_path = file_path
+        self._file_version = file_version
+
+    def get_file_path(self):
+        """Returns the path to the file relative to vim.
+
+        :returns: The path to the file as a string
+        """
+        return self._file_path
+
+    def get_file_version(self):
+        """Returns the version of the file (base 0).
+
+        :returns: The version of the file as an int
+        """
+        return self._file_version
+
+    def to_packet(self):
+        packet = super().to_packet()
+        packet.get_metadata().set_value(
+            MESSAGE_METADATA_FILE_VERSION, self._file_version)
+        packet.get_content().set_data(self._file_path)
+        return packet
+
+    @staticmethod
+    def from_packet(packet):
+        return UpdateFileStartRequestMessage(
+            id=packet.get_header().get_id(),
+            username=packet.get_header().get_username(),
+            session=packet.get_header().get_session(),
+            file_path=packet.get_content().get_data(),
+            file_version=packet.get_metadata().get_value(MESSAGE_METADATA_FILE_VERSION),
+            parent=None,
+        )
+
+
+class UpdateFileStartResponseMessage(BaseResponseMessage):
+    _type = MESSAGE_TYPE_UPDATE_FILE_START
+    _file_path = None
+    _file_version = None
+
+    def __init__(
+        self,
+        id=str(uuid4()),
+        username=MESSAGE_DEFAULT_USERNAME,
+        session=MESSAGE_DEFAULT_SESSION,
+        file_path=MESSAGE_DEFAULT_FILE_PATH,
+        file_version=MESSAGE_DEFAULT_FILE_VERSION,
+        parent=None
+    ):
+        super().__init__(id, username, session, parent)
+        self._file_path = file_path
+        self._file_version = file_version
+
+    def get_file_path(self):
+        """Returns the path to the file relative to vim.
+
+        :returns: The path to the file as a string
+        """
+        return self._file_path
+
+    def get_file_version(self):
+        """Returns the next version of the file should the data be fully sent
+        by the client and received by the server.
+
+        :returns: The version of the file as an int
+        """
+        return self._file_version
+
+    def to_packet(self):
+        packet = super().to_packet()
+        packet.get_metadata().set_value(
+            MESSAGE_METADATA_FILE_VERSION, self._file_version)
+        packet.get_content().set_data(self._file_path)
+        return packet
+
+    @staticmethod
+    def from_packet(packet):
+        return UpdateFileStartResponseMessage(
+            id=packet.get_header().get_id(),
+            username=packet.get_header().get_username(),
+            session=packet.get_header().get_session(),
+            file_path=packet.get_content().get_data(),
+            file_version=packet.get_metadata().get_value(MESSAGE_METADATA_FILE_VERSION),
+            parent=None,
+        )
+
+
+class UpdateFileDataRequestMessage(BaseRequestMessage):
+    _type = MESSAGE_TYPE_UPDATE_FILE_DATA
+    _file_length = None
+    _file_version = None
+    _total_chunks = None
+    _chunk_index = None
+    _chunk_data = None
+
+    def __init__(
+        self,
+        id=str(uuid4()),
+        username=MESSAGE_DEFAULT_USERNAME,
+        session=MESSAGE_DEFAULT_SESSION,
+        file_length=MESSAGE_DEFAULT_FILE_LENGTH,
+        file_version=MESSAGE_DEFAULT_FILE_VERSION,
+        total_chunks=MESSAGE_DEFAULT_TOTAL_CHUNKS,
+        chunk_index=MESSAGE_DEFAULT_CHUNK_INDEX,
+        chunk_data=MESSAGE_DEFAULT_CHUNK_DATA,
+        parent=None
+    ):
+        super().__init__(id, username, session, parent)
+        self._file_length = file_length
+        self._file_version = file_version
+        self._total_chunks = total_chunks
+        self._chunk_index = chunk_index
+        self._chunk_data = chunk_data
+
+    def get_file_length(self):
+        """Returns the total length of the file (not just this chunk).
+
+        :returns: The total length of the file
+        """
+        return self._file_length
+
+    def get_file_version(self):
+        """Returns the version of the file (base 0).
+
+        :returns: The version of the file as an int
+        """
+        return self._file_version
+
+    def get_total_chunks(self):
+        """Returns the total number of chunks that are being sent for the file.
+
+        :returns: The total number of chunks as an integer
+        """
+        return self._total_chunks
+
+    def get_chunk_index(self):
+        """Returns the position of this chunk (base 0) within the file.
+
+        :returns: The position of this chunk as an integer (base 0)
+        """
+        return self._chunk_index
+
+    def get_chunk_data(self):
+        """Returns the chunk of data representing part/all of the file.
+
+        :returns: The chunk of data as bytes
+        """
+        return self._chunk_data
+
+    def to_packet(self):
+        packet = super().to_packet()
+        (packet.get_metadata()
+         .set_value(MESSAGE_METADATA_CHUNK_INDEX, self._chunk_index)
+         .set_value(MESSAGE_METADATA_TOTAL_CHUNKS, self._total_chunks)
+         .set_value(MESSAGE_METADATA_FILE_VERSION, self._file_version)
+         .set_value(MESSAGE_METADATA_FILE_LENGTH, self._file_length))
+        packet.get_content().set_data(self._chunk_data)
+        return packet
+
+    @staticmethod
+    def from_packet(packet):
+        return UpdateFileDataRequestMessage(
+            id=packet.get_header().get_id(),
+            username=packet.get_header().get_username(),
+            session=packet.get_header().get_session(),
+            file_length=packet.get_metadata().get_value(MESSAGE_METADATA_FILE_LENGTH),
+            file_version=packet.get_metadata().get_value(MESSAGE_METADATA_FILE_VERSION),
+            chunk_index=packet.get_metadata().get_value(MESSAGE_METADATA_CHUNK_INDEX),
+            total_chunks=packet.get_metadata().get_value(MESSAGE_METADATA_TOTAL_CHUNKS),
+            chunk_data=packet.get_content().get_data(),
+            parent=None,
+        )
+
+
+class UpdateFileDataResponseMessage(BaseResponseMessage):
+    _type = MESSAGE_TYPE_UPDATE_FILE_DATA
+    _file_version = None
+    _chunks_received = None
+    _total_chunks = None
+
+    def __init__(
+        self,
+        id=str(uuid4()),
+        username=MESSAGE_DEFAULT_USERNAME,
+        session=MESSAGE_DEFAULT_SESSION,
+        file_version=MESSAGE_DEFAULT_FILE_VERSION,
+        chunks_received=MESSAGE_DEFAULT_CHUNKS_RECEIVED,
+        total_chunks=MESSAGE_DEFAULT_TOTAL_CHUNKS,
+        parent=None
+    ):
+        super().__init__(id, username, session, parent)
+        self._file_version = file_version
+        self._chunks_received = chunks_received
+        self._total_chunks = total_chunks
+
+    def get_file_version(self):
+        """Returns the candidate next version of file. If all chunks have
+        been received, this version represents the new file version.
+
+        :returns: The version as an integer
+        """
+        return self._file_version
+
+    def get_chunks_received(self):
+        """Returns the total number of chunks that have been received
+        thus far.
+
+        :returns: The total number of chunks
+        """
+        return self._chunks_received
+
+    def get_total_chunks(self):
+        """Returns the total number of chunks that are expected.
+
+        :returns: The total number of chunks as an integer
+        """
+        return self._total_chunks
+
+    def to_packet(self):
+        packet = super().to_packet()
+        (packet.get_metadata()
+         .set_value(MESSAGE_METADATA_FILE_VERSION, self._file_version)
+         .set_value(MESSAGE_METADATA_TOTAL_CHUNKS, self._total_chunks))
+        packet.get_content().set_data(self._chunks_received)
+        return packet
+
+    @staticmethod
+    def from_packet(packet):
+        return UpdateFileDataResponseMessage(
+            id=packet.get_header().get_id(),
+            username=packet.get_header().get_username(),
+            session=packet.get_header().get_session(),
+            file_version=packet.get_metadata().get_value(MESSAGE_METADATA_FILE_VERSION),
+            total_chunks=packet.get_metadata().get_value(MESSAGE_METADATA_TOTAL_CHUNKS),
+            chunks_received=packet.get_content().get_data(),
             parent=None,
         )
 
--- test/rplugin/python3/remote/messages/test_file.py
+++ test/rplugin/python3/remote/messages/test_file.py
@@ -240,6 +240,258 @@ class TestRetrieveFileResponseMessage(object):
         assert m.get_chunk_data() == self._chunk_data
 
 
+class TestUpdateFileDataRequestMessage(object):
+    _class = UpdateFileDataRequestMessage
+    _file_length = 999
+    _file_version = 7
+    _total_chunks = 99
+    _chunk_index = 12
+    _chunk_data = b'chunk data'
+
+    def test_constructor(self):
+        m = self._class(
+            id=TEST_ID,
+            username=TEST_USERNAME,
+            session=TEST_SESSION,
+            file_length=self._file_length,
+            file_version=self._file_version,
+            total_chunks=self._total_chunks,
+            chunk_index=self._chunk_index,
+            chunk_data=self._chunk_data,
+        )
+        assert m.get_id() == TEST_ID
+        assert m.get_type() == MESSAGE_TYPE_UPDATE_FILE_DATA
+        assert m.get_username() == TEST_USERNAME
+        assert m.get_session() == TEST_SESSION
+        assert m.get_file_length() == self._file_length
+        assert m.get_file_version() == self._file_version
+        assert m.get_total_chunks() == self._total_chunks
+        assert m.get_chunk_index() == self._chunk_index
+        assert m.get_chunk_data() == self._chunk_data
+
+    def test_to_packet(self):
+        m = self._class(
+            id=TEST_ID,
+            username=TEST_USERNAME,
+            session=TEST_SESSION,
+            file_length=self._file_length,
+            file_version=self._file_version,
+            total_chunks=self._total_chunks,
+            chunk_index=self._chunk_index,
+            chunk_data=self._chunk_data,
+        )
+        packet = m.to_packet()
+
+        assert packet.get_header().get_id() == TEST_ID
+        assert packet.get_header().get_type() == MESSAGE_TYPE_UPDATE_FILE_DATA
+        assert packet.get_header().get_username() == TEST_USERNAME
+        assert packet.get_header().get_session() == TEST_SESSION
+        assert packet.get_metadata().get_value(MESSAGE_SUBTYPE) == MESSAGE_SUBTYPE_REQUEST
+        assert packet.get_metadata().get_value(MESSAGE_METADATA_FILE_LENGTH) == self._file_length
+        assert packet.get_metadata().get_value(MESSAGE_METADATA_FILE_VERSION) == self._file_version
+        assert packet.get_metadata().get_value(MESSAGE_METADATA_TOTAL_CHUNKS) == self._total_chunks
+        assert packet.get_metadata().get_value(MESSAGE_METADATA_CHUNK_INDEX) == self._chunk_index
+        assert packet.get_content().get_data() == self._chunk_data
+
+    def test_from_packet(self):
+        packet = (Packet()
+                  .set_header(Header()
+                              .set_id(TEST_ID)
+                              .set_username(TEST_USERNAME)
+                              .set_session(TEST_SESSION))
+                  .set_metadata(Metadata()
+                                .set_value(MESSAGE_METADATA_FILE_LENGTH, self._file_length)
+                                .set_value(MESSAGE_METADATA_FILE_VERSION, self._file_version)
+                                .set_value(MESSAGE_METADATA_TOTAL_CHUNKS, self._total_chunks)
+                                .set_value(MESSAGE_METADATA_CHUNK_INDEX, self._chunk_index))
+                  .set_content(Content().set_data(self._chunk_data)))
+        m = self._class.from_packet(packet)
+
+        assert m.get_id() == TEST_ID
+        assert m.get_username() == TEST_USERNAME
+        assert m.get_session() == TEST_SESSION
+        assert m.get_file_length() == self._file_length
+        assert m.get_file_version() == self._file_version
+        assert m.get_total_chunks() == self._total_chunks
+        assert m.get_chunk_index() == self._chunk_index
+        assert m.get_chunk_data() == self._chunk_data
+
+
+class TestUpdateFileStartRequestMessage(object):
+    _class = UpdateFileStartRequestMessage
+    _file_path = 'path/to/file'
+    _file_version = 7
+
+    def test_constructor(self):
+        m = self._class(
+            id=TEST_ID,
+            username=TEST_USERNAME,
+            session=TEST_SESSION,
+            file_path=self._file_path,
+            file_version=self._file_version,
+        )
+        assert m.get_id() == TEST_ID
+        assert m.get_type() == MESSAGE_TYPE_UPDATE_FILE_START
+        assert m.get_username() == TEST_USERNAME
+        assert m.get_session() == TEST_SESSION
+        assert m.get_file_path() == self._file_path
+        assert m.get_file_version() == self._file_version
+
+    def test_to_packet(self):
+        m = self._class(
+            id=TEST_ID,
+            username=TEST_USERNAME,
+            session=TEST_SESSION,
+            file_path=self._file_path,
+            file_version=self._file_version,
+        )
+        packet = m.to_packet()
+
+        assert packet.get_header().get_id() == TEST_ID
+        assert packet.get_header().get_type() == MESSAGE_TYPE_UPDATE_FILE_START
+        assert packet.get_header().get_username() == TEST_USERNAME
+        assert packet.get_header().get_session() == TEST_SESSION
+        assert packet.get_metadata().get_value(MESSAGE_SUBTYPE) == MESSAGE_SUBTYPE_REQUEST
+        assert packet.get_metadata().get_value(MESSAGE_METADATA_FILE_VERSION) == self._file_version
+        assert packet.get_content().get_data() == self._file_path
+
+    def test_from_packet(self):
+        packet = (Packet()
+                  .set_header(Header()
+                              .set_id(TEST_ID)
+                              .set_username(TEST_USERNAME)
+                              .set_session(TEST_SESSION))
+                  .set_metadata(Metadata()
+                                .set_value(MESSAGE_METADATA_FILE_VERSION, self._file_version))
+                  .set_content(Content().set_data(self._file_path)))
+        m = self._class.from_packet(packet)
+
+        assert m.get_id() == TEST_ID
+        assert m.get_username() == TEST_USERNAME
+        assert m.get_session() == TEST_SESSION
+        assert m.get_file_path() == self._file_path
+        assert m.get_file_version() == self._file_version
+
+
+class TestUpdateFileStartResponseMessage(object):
+    _class = UpdateFileStartResponseMessage
+    _file_path = 'path/to/file'
+    _file_version = 7
+
+    def test_constructor(self):
+        m = self._class(
+            id=TEST_ID,
+            username=TEST_USERNAME,
+            session=TEST_SESSION,
+            file_path=self._file_path,
+            file_version=self._file_version,
+        )
+        assert m.get_id() == TEST_ID
+        assert m.get_type() == MESSAGE_TYPE_UPDATE_FILE_START
+        assert m.get_username() == TEST_USERNAME
+        assert m.get_session() == TEST_SESSION
+        assert m.get_file_path() == self._file_path
+        assert m.get_file_version() == self._file_version
+
+    def test_to_packet(self):
+        m = self._class(
+            id=TEST_ID,
+            username=TEST_USERNAME,
+            session=TEST_SESSION,
+            file_path=self._file_path,
+            file_version=self._file_version,
+        )
+        packet = m.to_packet()
+
+        assert packet.get_header().get_id() == TEST_ID
+        assert packet.get_header().get_type() == MESSAGE_TYPE_UPDATE_FILE_START
+        assert packet.get_header().get_username() == TEST_USERNAME
+        assert packet.get_header().get_session() == TEST_SESSION
+        assert packet.get_metadata().get_value(MESSAGE_SUBTYPE) == MESSAGE_SUBTYPE_RESPONSE
+        assert packet.get_metadata().get_value(MESSAGE_METADATA_FILE_VERSION) == self._file_version
+        assert packet.get_content().get_data() == self._file_path
+
+    def test_from_packet(self):
+        packet = (Packet()
+                  .set_header(Header()
+                              .set_id(TEST_ID)
+                              .set_username(TEST_USERNAME)
+                              .set_session(TEST_SESSION))
+                  .set_metadata(Metadata()
+                                .set_value(MESSAGE_METADATA_FILE_VERSION, self._file_version))
+                  .set_content(Content().set_data(self._file_path)))
+        m = self._class.from_packet(packet)
+
+        assert m.get_id() == TEST_ID
+        assert m.get_username() == TEST_USERNAME
+        assert m.get_session() == TEST_SESSION
+        assert m.get_file_path() == self._file_path
+        assert m.get_file_version() == self._file_version
+
+
+class TestUpdateFileDataResponseMessage(object):
+    _class = UpdateFileDataResponseMessage
+    _file_version = 7
+    _total_chunks = 99
+    _chunks_received = 50
+
+    def test_constructor(self):
+        m = self._class(
+            id=TEST_ID,
+            username=TEST_USERNAME,
+            session=TEST_SESSION,
+            file_version=self._file_version,
+            total_chunks=self._total_chunks,
+            chunks_received=self._chunks_received,
+        )
+        assert m.get_id() == TEST_ID
+        assert m.get_type() == MESSAGE_TYPE_UPDATE_FILE_DATA
+        assert m.get_username() == TEST_USERNAME
+        assert m.get_session() == TEST_SESSION
+        assert m.get_file_version() == self._file_version
+        assert m.get_total_chunks() == self._total_chunks
+        assert m.get_chunks_received() == self._chunks_received
+
+    def test_to_packet(self):
+        m = self._class(
+            id=TEST_ID,
+            username=TEST_USERNAME,
+            session=TEST_SESSION,
+            file_version=self._file_version,
+            total_chunks=self._total_chunks,
+            chunks_received=self._chunks_received,
+        )
+        packet = m.to_packet()
+
+        assert packet.get_header().get_id() == TEST_ID
+        assert packet.get_header().get_type() == MESSAGE_TYPE_UPDATE_FILE_DATA
+        assert packet.get_header().get_username() == TEST_USERNAME
+        assert packet.get_header().get_session() == TEST_SESSION
+        assert packet.get_metadata().get_value(MESSAGE_SUBTYPE) == MESSAGE_SUBTYPE_RESPONSE
+        assert packet.get_metadata().get_value(MESSAGE_METADATA_FILE_VERSION) == self._file_version
+        assert packet.get_metadata().get_value(MESSAGE_METADATA_TOTAL_CHUNKS) == self._total_chunks
+        assert packet.get_content().get_data() == self._chunks_received
+
+    def test_from_packet(self):
+        packet = (Packet()
+                  .set_header(Header()
+                              .set_id(TEST_ID)
+                              .set_username(TEST_USERNAME)
+                              .set_session(TEST_SESSION))
+                  .set_metadata(Metadata()
+                                .set_value(MESSAGE_METADATA_FILE_VERSION, self._file_version)
+                                .set_value(MESSAGE_METADATA_TOTAL_CHUNKS, self._total_chunks))
+                  .set_content(Content().set_data(self._chunks_received)))
+        m = self._class.from_packet(packet)
+
+        assert m.get_id() == TEST_ID
+        assert m.get_username() == TEST_USERNAME
+        assert m.get_session() == TEST_SESSION
+        assert m.get_file_version() == self._file_version
+        assert m.get_total_chunks() == self._total_chunks
+        assert m.get_chunks_received() == self._chunks_received
+
+
 class TestFileChangeBroadcastMessage(object):
     _class = FileChangeBroadcastMessage
     _file_path = 'path/to/file'
