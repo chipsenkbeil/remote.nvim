80ea3fd2055eb752d6970b7fa008a86ad054b535
Chip Senkbeil <chip.senkbeil@gmail.com>
2018-05-05 11:15:27 -0500

    Added tests for registry

--- test/rplugin/python3/remote/test_registry.py
+++ test/rplugin/python3/remote/test_registry.py
@@ -79,3 +79,35 @@ class TestMessageRegistry(object):
         assert mr.lookup(TEST_TYPE, MESSAGE_SUBTYPE_REQUEST) is None
         assert mr.lookup(TEST_TYPE, MESSAGE_SUBTYPE_RESPONSE) is None
         assert mr.lookup(TEST_TYPE, MESSAGE_SUBTYPE_BROADCAST) == M
+
+
+class TestActionRegistry(object):
+    def test_register(self):
+        class A(object):
+            _fish = 999
+
+        def my_action(a):
+            return a
+
+        ar = ActionRegistry()
+        ar.register(A, my_action)
+
+        assert ar.lookup(A()) == my_action
+
+    def test_process(self):
+        class A(object):
+            def __init__(self):
+                self.value = 999
+
+        a_instance = A()
+
+        def my_action(a):
+            old_value = a.value
+            a.value = 1000
+            return old_value
+
+        ar = ActionRegistry()
+        ar.register(A, my_action)
+
+        assert ar.process(a_instance) == 999
+        assert a_instance.value == 1000
