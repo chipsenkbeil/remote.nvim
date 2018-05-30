910e5f91019f5080322180f8608e2961eb471f86
Chip Senkbeil <chipsenkbeil@fb.com>
2018-05-30 10:54:26 -0500

    Finished timer code

--- rplugin/python3/remote/timer.py
+++ rplugin/python3/remote/timer.py
@@ -13,6 +13,9 @@ class Timer(object):
         :param loop: The event loop to use when setting up the timer
         :param interval: The number of seconds to wait inbetween handler calls,
                          serving as a minimum (can take more time)
+        :param stop_on_exception: If True, will stop the timer if an exception
+                                  occurs, otherwise will log exception
+                                  internally
         """
         self._loop = loop
         self._running = False
@@ -20,6 +23,8 @@ class Timer(object):
         self._exceptions = []
         self._stop_on_exception = stop_on_exception
         self._handler = None
+        self._limit = -1
+        self._count = 0
 
     def set_handler(self, handler, *args, **kwargs):
         """Sets the handler to be invoked by the timer.
@@ -39,9 +44,15 @@ class Timer(object):
                 except Exception as ex:
                     if self._stop_on_exception:
                         self.stop()
+                        break
                     else:
                         self._exceptions.append(ex)
                 finally:
+                    self._count += 1
+                    if (self._limit >= 0 and self._count >= self._limit):
+                        self.stop()
+                        break
+
                     end_time = self._loop.time()
                     delay = max(0.0, self._interval - (end_time - start_time))
                     await asyncio.sleep(delay, loop=self._loop)
@@ -56,6 +67,7 @@ class Timer(object):
         """
         assert self._handler is not None, 'Handler has not been set!'
         self._running = True
+        self._count = 0
         self._loop.create_task(self._handler())
         return self
 
@@ -96,3 +108,26 @@ class Timer(object):
         """
         self._exceptions = []
         return self
+
+    def set_limit(self, limit):
+        """Sets the maximum number of invocations a timer will run the handler.
+
+        :param limit: The limit as an integer, or -1 if no limit should be used
+        :returns: The updated timer
+        """
+        self._limit = limit
+        return self
+
+    def limit(self):
+        """Returns the limit set on the timer.
+
+        :returns: The limit as an integer
+        """
+        return self._limit
+
+    def count(self):
+        """Returns the number of times the timer has run.
+
+        :returns: The total number of times the timer has invoked the handler
+        """
+        return self._count
--- test/rplugin/python3/remote/test_timer.py
+++ test/rplugin/python3/remote/test_timer.py
@@ -24,21 +24,50 @@ class TestTimer(object):
     @pytest.mark.asyncio
     async def test_timecheck(self, timer):
         expected = 3
+        variance = 1
 
         f = Mock(return_value=None)
-        timer.set_handler(f, TEST_ARGS, TEST_KWARGS).start()
+        timer.set_handler(f, *TEST_ARGS, **TEST_KWARGS).start()
 
-        delay = (TEST_INTVL * expected) * 2
+        delay = TEST_INTVL * expected
         await asyncio.sleep(delay)
 
         actual = f.call_count
-        assert actual == expected
+        assert expected - variance <= actual, (
+            '{} lower than expected with variance'.format(actual))
+        assert expected + variance >= actual, (
+            '{} higher than expected with variance'.format(actual))
+
+    @pytest.mark.asyncio
+    async def test_count_update(self, timer):
+        f = Mock(return_value=None)
+        timer.set_handler(f, *TEST_ARGS, **TEST_KWARGS).start()
+
+        await asyncio.sleep(TEST_INTVL * 5)
+        timer.stop()
+        await asyncio.sleep(TEST_INTVL)
+
+        assert f.call_count == timer.count()
+
+    @pytest.mark.asyncio
+    async def test_limit_enforced(self, timer):
+        expected = 3
+
+        f = Mock(return_value=None)
+        timer.set_handler(
+            f, *TEST_ARGS, **TEST_KWARGS
+        ).set_limit(expected).start()
+
+        await asyncio.sleep(TEST_INTVL * expected * 3)
+
+        assert not timer.running(), 'Timer did not reach limit yet!'
+        assert f.call_count == timer.limit()
 
     @pytest.mark.asyncio
     async def test_exceptions(self, timer):
         f = Mock(return_value=None)
         f.side_effect = Exception('Test Failure')
-        timer.set_handler(f, TEST_ARGS, TEST_KWARGS)
+        timer.set_handler(f, *TEST_ARGS, **TEST_KWARGS)
 
         assert len(timer.exceptions()) == 0
         timer.start()
@@ -48,10 +77,22 @@ class TestTimer(object):
         assert timer.exceptions()[0] == f.side_effect
 
     @pytest.mark.asyncio
+    async def test_invoked_with_arguments(self, timer):
+        f = Mock(return_value=None)
+        timer.set_handler(f, *TEST_ARGS, **TEST_KWARGS)
+
+        assert len(timer.exceptions()) == 0
+        timer.start()
+
+        await asyncio.sleep(TEST_INTVL)
+
+        f.assert_called_with(*TEST_ARGS, **TEST_KWARGS)
+
+    @pytest.mark.asyncio
     async def test_clear_exceptions(self, timer):
         f = Mock(return_value=None)
         f.side_effect = Exception('Test Failure')
-        timer.set_handler(f, TEST_ARGS, TEST_KWARGS).start()
+        timer.set_handler(f, *TEST_ARGS, **TEST_KWARGS).start()
 
         await asyncio.sleep(TEST_INTVL)
         assert len(timer.exceptions()) > 0
@@ -64,15 +105,27 @@ class TestTimer(object):
 
     def test_running_true_after_start(self, timer):
         f = Mock(return_value=None)
-        timer.set_handler(f, TEST_ARGS, TEST_KWARGS).start()
+        timer.set_handler(f, *TEST_ARGS, **TEST_KWARGS).start()
 
         assert timer.running()
 
     def test_running_false_after_stop(self, timer):
         f = Mock(return_value=None)
-        timer.set_handler(f, TEST_ARGS, TEST_KWARGS).start().stop()
+        timer.set_handler(f, *TEST_ARGS, **TEST_KWARGS).start().stop()
 
         assert not timer.running()
 
     def test_interval(self, timer):
         assert timer.interval() == TEST_INTVL
+
+    def test_limit_default(self, timer):
+        assert timer.limit() == -1
+
+    def test_limit_changed(self, timer):
+        expected = 999
+        timer.set_limit(expected)
+        actual = timer.limit()
+        assert actual == expected
+
+    def test_count_default(self, timer):
+        assert timer.count() == 0
